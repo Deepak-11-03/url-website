e2e2eaeb8365fcdc6c9cad44c0bf16fdab67ff96
Deepak-11-03 <jk449738@gmail.com>
2022-07-21 18:54:57 +0530

    env

---
+++ index.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const express= require('express');
+const dotenv=require('dotenv')
+const route= require('./routes/routes');
+const app = express();
+
+dotenv.config({ path:'./config.env'})
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({extended:true}));
+
+app.use(express.static('public'));
+
+
+const DB =process.env.DATABASE
+
+mongoose.connect(DB,
+{useNewUrlparser:true})
+.then(()=>console.log("mongodb is connected"))
+.catch(err=>console.log(err))
+
+app.set('view engine', 'hbs');
+app.get('/',  (req, res)=>{
+    return res.render('index');
+})
+app.use('/',route);
+
+app.listen(process.env.PORT,(err)=> {
+    console.log(`connected to ${process.env.PORT}`)
+})
---
+++ routes/routes.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const urlModel = require('../Model/urlModel');
+const shortid = require('shortid');
+const router = express.Router();
+
+const {promisify}= require('util')
+const redisClient=require('../redis/redis')
+
+
+//Connection setup for redis
+
+const SET_ASYNC = promisify(redisClient.SET).bind(redisClient);
+const GET_ASYNC = promisify(redisClient.GET).bind(redisClient);
+
+
+
+router.post('/url', async (req, res) => {
+    try {
+        let longUrl = req.body.longUrl;
+
+        const baseUrl = "https://url-0.herokuapp.com";
+        const urlCode = shortid.generate();
+        const shortUrl = baseUrl + '/' + urlCode;
+
+        const urlPresent = await urlModel.findOne({ longUrl });
+        if (urlPresent) {
+            return res.status(200).render('index', { "shortUrl": urlPresent.shortUrl });
+        }
+        let data = new urlModel({ longUrl, shortUrl, urlCode });
+        const shortenUrl = await urlModel.create(data);
+
+        await SET_ASYNC(`${shortenUrl}`, JSON.stringify(shortUrl))
+        return res.status(201).render('index', { "shortUrl": shortUrl });
+    } 
+    catch (err) {
+        return res.status(500).send({ status: false, error: err.message });
+    }
+});
+
+router.get('/:urlcode', async (req, res) => {
+    try {
+        let urlCode = req.params.urlcode;
+
+        let cachedUrlCode = await GET_ASYNC(`${urlCode}`)
+        if(cachedUrlCode){
+            let parseUrl = JSON.parse(cachedUrlCode)
+                let cachedLongUrl = parseUrl.longUrl
+                return res.status(302).redirect(cachedLongUrl)
+        }
+        let getUrl = await urlModel.findOne({ urlCode });
+        if (!getUrl) {
+            return res.status(404).send({ msg: "url not found" });
+        }
+        let url = getUrl.longUrl;
+        await SET_ASYNC(`${urlCode}`, JSON.stringify(getUrl))
+        return res.status(302).redirect(url);
+    } 
+    catch (err) {
+        return res.status(500).send({ error: err.message });
+    }
+});
+
+
+module.exports = router
