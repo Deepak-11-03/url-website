f519fe5b66192cae62281dcce754e518f1ecbddf
unknown <jk449738@gmailcom>
2022-08-30 22:54:13 +0530

    keyboard bug fix

--- routes/routes.js
+++ routes/routes.js
@@ -3,20 +3,7 @@ const urlModel = require('../Model/urlModel');
 const shortid = require('shortid');
 const router = express.Router();
 const {promisify}= require('util')
-const redis = require('redis')
-
-const redisClient = redis.createClient(
-    10269,
-    "redis-10269.c264.ap-south-1-1.ec2.cloud.redislabs.com",
-    { no_ready_check: true }
-);
-redisClient.auth("YLY1q832YZYVk5eRkQouNN03ycLQbLVD", function (err) {
-    if (err) throw err;
-});
-
-redisClient.on("connect", async function () {
-    console.log("Connected to Redis..");
-});
+const redisClient =require('../redis')
 
 
 //Connection setup for redis
